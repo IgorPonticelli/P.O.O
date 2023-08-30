18eef268a18915004437d05a36939f2ab95226e5
Igor <igorponticelli8@gmail.com>
2023-08-30 09:42:05 -0300

    POO__cofrinho

--- Exercicio21/Cofrinho.java
+++ Exercicio21/Cofrinho.java
@@ -1,13 +1,46 @@
 package Exercicio21;
 
 public class Cofrinho{
-    public 
-    public boolean insere(Moeda moeda){
+    private final int capacidade = 30;
+    private Moeda [] base;
+    private int aux;
 
+    public Cofrinho(){
+        base = new Moeda[capacidade];
+        aux = 0;
+    }
+
+    public int tamanho(){
+        return aux;
+    }
+
+    public int capacidade(){
+        return capacidade;
+    }
+
+
+    public boolean insere(Moeda moeda){
+        if(aux == capacidade){
+            return false;
+        }else{
+            base [aux] = moeda;
+            aux++;
+            return true;
+
+        }
 
     }
 
-    public Moeda retira(){
+    public boolean retira(int indice){
+        if(indice<0 || indice>=aux ){
+            return false;
+        }else{
+            for(int i = indice; i<aux -1; i++){
+                base[i] = base[i +1];
+            }
+            aux--;
+            return true;
+        }
         
     }
     
