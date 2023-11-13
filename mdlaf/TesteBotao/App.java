eb0c0f7a53baf3c87e7e432d30ca46309d888d40
Igor <igorponticelli8@gmail.com>
2023-11-13 13:02:52 -0300

    Teste2

--- mdlaf/TesteBotao/App.java
+++ mdlaf/TesteBotao/App.java
@@ -1,5 +1,32 @@
 package mdlaf.TesteBotao;
 
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
 public class App {
-    
+    DropShadowBorder teste = new DropShadowBorder();
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Botão Legal!"); // Jframe é uma janela
+        //Janela = apresenta a interface da aplicação
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container contentPane = frame.getContentPane();
+         contentPane.setLayout(new FlowLayout());//Cada componente sabe o seu tamanho
+         //Container serve para agrupar e exibir componentes
+        
+        JLabel label = new JLabel("Olha só esse botão: ");
+        contentPane.add(label);
+        //Painel = intermediário que possui um layout para os seus componentes.
+
+        JButton button = new JButton("Não clica");
+        //Cria um botão
+
+       
+
+        contentPane.add(button);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
+
